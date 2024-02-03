9ec6687a4f11cf91dccb38a1fd06e464c6b44a75
RoosterMonkey777 <94876933+RoosterMonkey777@users.noreply.github.com>
2024-02-03 12:53:09 -0500

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,28 @@
+# - Python Lists are ordered collections of data just like arrays (in other programming languages)
+# - Allows for different types of elements to be in the list
+# - Similar to Vectors in C++ and ArrayList in Java
+# - Costly operation is 'inserting' or 'deleting', from the beginning of the list, as all subsequent elements need to be shifted 
+
+
+List = [1,2,3,"GFG", 2.3]
+print(List)
+
+# - Lists are simples containers and need not be homogeneous
+# - Lists are mutable (they can be altered after creation)
+
+# Python Program to demonstrate 
+
+List = []
+print("Blank List:" )
+print(List)
+
+# Creating list of numbers
+List = [1,2,3,4,5]
+print('\nList of numbers: ')
+print(List)
+
+# Creating list of strings and accessing using index
+List = ["Hello", "Cheese", "Goat"]
+print('\nList Items: ')
+print(List[0])
+print(List[2])
